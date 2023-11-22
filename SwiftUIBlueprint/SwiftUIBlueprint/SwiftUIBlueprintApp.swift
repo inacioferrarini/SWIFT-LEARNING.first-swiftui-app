8963e715e51342c5db76a14e2e659e2fcadd0a26
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2023-11-22 10:21:52 -0300

    Adds project scaffold

---
+++ SwiftUIBlueprint/SwiftUIBlueprint/SwiftUIBlueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBlueprintApp.swift
+//  SwiftUIBlueprint
+//
+//  Created by José Inácio Athayde Ferrarini on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBlueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
