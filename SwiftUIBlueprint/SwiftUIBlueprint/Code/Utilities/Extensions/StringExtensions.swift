d26c30922a15eaf6a6e9ec72970c654927a77299
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2023-11-22 11:18:19 -0300

    Adds Swiftlint

--- SwiftUIBlueprint/SwiftUIBlueprint/Code/Utilities/Extensions/StringExtensions.swift
+++ SwiftUIBlueprint/SwiftUIBlueprint/Code/Utilities/Extensions/StringExtensions.swift
@@ -4,7 +4,7 @@ extension String {
     var localized: String {
         return NSLocalizedString(self, comment: "")
     }
-    
+
     func localized(arguments: CVarArg...) -> String {
         return String(format: self.localized, arguments: arguments)
     }
