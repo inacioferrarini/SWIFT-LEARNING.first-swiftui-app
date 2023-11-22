286fe865e68c08f139f57f7450bdd46f9f59d888
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2023-11-22 11:09:17 -0300

    Adds localized text support

--- SwiftUIBlueprint/SwiftUIBlueprint/ContentView.swift
+++ SwiftUIBlueprint/SwiftUIBlueprint/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("ContentView.WelcomeMessage".localized(arguments: "Inácio"))
         }
         .padding()
     }
